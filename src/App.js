8422a621cba780df468d9b39b3e7c4afe8985036
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-19 20:04:41 -0300

    Requisite 1, 2 and 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import Content from './Content';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
-    <h1>Exercise - Frontend Content</h1>
+    <>
+      <h1>Exercise - Frontend content</h1>
+      <Header />
+      <Content />
+      <Footer />
+    </>
   );
 }
 
