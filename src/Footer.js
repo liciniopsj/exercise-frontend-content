79cecf58256e9324ce778f7ad082b35dfc40c98f
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-19 20:12:20 -0300

    Requisite 3 fix

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Footer extends React.Component {
   render() {
     return (
-      <h3 data-testid="footer">E isso é só o começo...</h3>
+      <footer data-testid="footer">
+        <h3>E isso é só o começo...</h3>
+      </footer>
     );
   }
 }
